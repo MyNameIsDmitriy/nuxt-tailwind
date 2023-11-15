bd82015d0cb2d9ed80708feed82126cad1d5e3e3
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2023-11-15 17:11:27 +0300

    ref: put svg into separate file

--- components/Header.vue
+++ components/Header.vue
@@ -7,37 +7,19 @@
 
       <nav class="font-primary text-2xl font-normal text-navigate">
         <div class="tablet:hidden" @click="toggleMenu">
-          <svg
+          <img
             v-if="!isPressed"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="#2672ab"
+            src="../assets/svg/burger-open.svg"
+            alt=""
             class="w-12 h-12 duration-300"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+          />
 
-          <svg
+          <img
             v-else
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="#2672ab"
+            src="../assets/svg/burger-close.svg"
+            alt=""
             class="w-12 h-12 rotate-45"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
+          />
         </div>
 
         <ul
--- components/LearnMoreButton.vue
+++ components/LearnMoreButton.vue
@@ -9,20 +9,7 @@
       class="flex items-center px-8 py-[1.125rem] rounded-lg bg-button hover:text-[#FFFFFF] active:translate-y-[-3px] active:shadow-xl"
     >
       <p class="pr-2">Узнать больше</p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-        />
-      </svg>
+      <img class="w-6 h-6" src="../assets/svg/arrow.svg" alt="arrow-right" />
     </button>
   </NuxtLink>
 </template>
